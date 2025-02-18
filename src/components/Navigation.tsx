9c17a2a7303063bdaf74ff77d9f591dd172bd0fe
Raj Lathigra <63877323+raj-1106@users.noreply.github.com>
2025-02-18 01:07:40 -0800

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,8 +6,8 @@ const Navigation = () => {
   
   const links = [
     { name: "About me", path: "/work" },
-    { name: "Work Experience", path: "/webflow" },
-    { name: "Projects", path: "/favorites" },
+    { name: "Projects", path: "/webflow" },
+    { name: "Work Experience", path: "/favorites" },
     { name: "Social", path: "/social" }
   ];
 
